660fd393c81458782b9e2f11d96bf663e0208878
Achref Ghozlene <medachrefg@gmail.com>
2024-06-20 15:28:27 +0100

    binding to Directive proprietes

--- src/app/project/directives/better-highlight.directive.ts
+++ src/app/project/directives/better-highlight.directive.ts
@@ -1,18 +1,21 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
+  @Input() defaultBackgroundColor: string = '';
+  @Input() customBackgroundColor: string = '';
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-
-
+    /* this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');*/
+    this.backgroundColor = this.defaultBackgroundColor;
   }
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    /* this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');*/
 
+    this.backgroundColor = this.customBackgroundColor;
 
   }
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
@@ -20,5 +23,6 @@ export class BetterHighlightDirective implements OnInit {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+    this.backgroundColor = this.defaultBackgroundColor;
   }
 }
