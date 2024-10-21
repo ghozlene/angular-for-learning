dbe6e19791ce6c5a06fb4583076eb66f72da6d4b
Achref Ghozlene <medachrefg@gmail.com>
2024-10-21 11:27:14 +0100

    validating user input

--- src/app/project/components/recipes/edit/edit.component.ts
+++ src/app/project/components/recipes/edit/edit.component.ts
@@ -51,16 +51,16 @@ export class EditComponent {
           recipeIngredients.push(
             new FormGroup({
               name: new FormControl(ingredient.name, Validators.required),
-              amount: new FormControl(ingredient.amount, [Validators.required, Validators.min(1)])
+              amount: new FormControl(ingredient.amount, [Validators.required, Validators.pattern(/^[1-9][0-9]*$/)])
             })
           );
         }
       }
     }
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'imagePath': new FormControl(imagePath),
-      'description': new FormControl(description),
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath': new FormControl(imagePath, Validators.required),
+      'description': new FormControl(description, Validators.required),
       "ingredients": recipeIngredients
     });
     console.log(this.recipeForm);
@@ -73,8 +73,8 @@ export class EditComponent {
   onAddIngredient() {
     (<FormArray> this.recipeForm.get('ingredients')).push(
       new FormGroup({
-        'name': new FormControl(),
-        'amount': new FormControl()
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9][0-9]*$/)])
       })
 
     );
