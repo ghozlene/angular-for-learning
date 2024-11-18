73832aaf0aced9253068f29b22094bca8546e2f8
Achref Ghozlene <medachrefg@gmail.com>
2024-11-18 14:30:55 +0100

    splitting the app into feature modules like for example Recipe Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Test1Component } from './exercices/test1/test1.component';
 import { ComponentsComponent } from './project/components/components.component';
 import { HeaderComponent } from './project/header/header.component';
-import { RecipesComponent } from './project/components/recipes/recipes.component';
-import { RecipeListComponent } from './project/components/recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './project/components/recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './project/components/recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './project/components/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './project/components/shopping-list/shopping-edit/shopping-edit.component';
 import { BasicHighlightDirective } from './project/directives/basic.highlight';
-import { BetterHighlightDirective } from './project/directives/better-highlight.directive';
 import { CustomDirectiveDirective } from './project/directives/custom-directive.directive';
 import { DropdownDirective } from './project/components/shared/dropdown.directive';
-import { RecipeDirective } from './project/components/services/recipe.directive';
 import { ShoppingListDirective } from './project/components/services/shopping-list.directive';
-import { RecipeStartComponent } from './project/recipes/recipe-start/recipe-start.component';
 import { EditComponent } from './project/components/recipes/edit/edit.component';
 import { Test2FormsComponent } from './components/exercices/test2-forms/test2-forms.component';
 import { ReactiveFormTestComponent } from './components/exercices/reactive-form-test/reactive-form-test.component';
@@ -33,6 +26,7 @@ import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinner } from './project/components/shared/loading-spinner/loading-spinner-component';
 import { AuthInterceptorService } from './auth/auth.interceptor.service';
 import { AlertComponent } from './project/components/shared/alert/alert.component';
+import { RecipeModule } from './project/components/recipes/recipe.module';
 
 @NgModule({
   declarations: [
@@ -42,19 +36,13 @@ import { AlertComponent } from './project/components/shared/alert/alert.componen
     Test1Component,
     HeaderComponent,
     ComponentsComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     BasicHighlightDirective,
-    BetterHighlightDirective,
+
     CustomDirectiveDirective,
     DropdownDirective,
-    RecipeDirective,
     ShoppingListDirective,
-    RecipeStartComponent,
     EditComponent,
     Test2FormsComponent,
     ReactiveFormTestComponent,
@@ -74,7 +62,8 @@ import { AlertComponent } from './project/components/shared/alert/alert.componen
     ReactiveFormsModule,
     AppRoutingModule,
     RecaptchaModule,
-    HttpClientModule
+    HttpClientModule,
+    RecipeModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
---
+++ src/app/project/components/recipes/recipe.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from "@angular/core";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeDirective } from "../services/recipe.directive";
+import { RecipeStartComponent } from "../../recipes/recipe-start/recipe-start.component";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { BetterHighlightDirective } from "../../directives/better-highlight.directive";
+
+
+
+@NgModule({
+
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeDirective,
+    RecipeStartComponent,
+    BetterHighlightDirective,
+  ],
+  imports: [
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeDirective,
+    RecipeStartComponent,
+    BetterHighlightDirective,
+  ],
+})
+export class RecipeModule { }
